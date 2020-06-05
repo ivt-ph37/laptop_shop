939a3884314a4ec09c1d19af968a47d2285f5c65
AnhMinh <cheanhminh1401@gmail.com>
2020-06-05 20:23:06 +0700

    create view user

---
+++ resources/views/master_user.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="{{asset('css/user/style.css')}}" rel="stylesheet" type="text/css" media="all"/>
+	<link href="{{asset('css/user/slider.css')}}" rel="stylesheet" type="text/css" media="all"/>
+	<link href="{{asset('css/user/easy-responsive-tabs.css')}}" rel="stylesheet" type="text/css" media="all"/>
+	<link rel="stylesheet" href="{{asset('css/user/global.css')}}">
+	<script type="text/javascript" src="{{asset('js/user/jquery-1.7.2.min.js')}}"></script> 
+	<script type="text/javascript" src="{{asset('js/user/move-top.js')}}"></script>
+	<script type="text/javascript" src="{{asset('js/user/easing.js')}}"></script>
+	<script type="text/javascript" src="{{asset('js/user/startstop-slider.js')}}"></script>
+	<script src="{{asset('js/user/easyResponsiveTabs.js')}}" type="text/javascript"></script>
+	<script src="{{asset('js/user/slides.min.jquery.js')}}"></script>
+</head>
+<body>
+	@yield('content')
+</body>
+</html>
